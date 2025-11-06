4fe6995a61ebc22de94f4f2a72ff0cdc615103a8
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-11-06 15:38:02 +0900

    pr3.2

--- main.c
+++ main.c
@@ -6,17 +6,22 @@
 int main(void)
 {
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int a[SIZE] = {1, 2, 3, 4, 5};
+	int b[SIZE] = {1, 2, 3, 4, 5};
+	int flag_same = 1;
 	
 	for (i = 0; i < SIZE; i++)
-		grade[i] = rand() % 100;
-	
-	for (i = 0; i < SIZE; i++)
-		grade[i] = score[i];
-	
-	for (i = 0; i < SIZE; i++)
-		printf("score[%d] = %d\n", i, score[i]);
+	{
+    if (a[i] != b[i]) {
+        printf("Different Values: index %d (a=%d, b=%d)\n", i, a[i], b[i]);
+        flag_same = 0;
+    	}
+	}
+
+	if (flag_same)
+    	printf("Arraies have same values.\n");
+	else
+    	printf("Arraies have different values.\n");
 	
 	return 0;
 }
