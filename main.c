e384d4d50a09e656af47e80fab2e2668ce8be31f
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-11-06 15:43:46 +0900

    pr4

--- main.c
+++ main.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 
+
+void square_array(int a[], int size);
+void print_array(int a[], int size);
 
 int main(void)
 {
+	int list[SIZE] = {1, 2, 3, 4};
+	
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);
+	
+	return 0;	
+}
+
+void square_array(int a[], int size)
+{
 	int i;
-	int a[SIZE] = {1, 2, 3, 4, 5};
-	int b[SIZE] = {1, 2, 3, 4, 5};
-	int flag_same = 1;
 	
 	for (i = 0; i < SIZE; i++)
-	{
-    if (a[i] != b[i]) {
-        printf("Different Values: index %d (a=%d, b=%d)\n", i, a[i], b[i]);
-        flag_same = 0;
-    	}
-	}
+		a[i] = a[i] * a[i];
+		
+}
 
-	if (flag_same)
-    	printf("Arraies have same values.\n");
-	else
-    	printf("Arraies have different values.\n");
+void print_array(int a[], int sizse)
+{
+	int i;
 	
-	return 0;
+	for (i = 0; i < SIZE; i++)
+		printf("%3d", a[i]);
+	
+	printf("\n");
 }
